77c17dafa62cd9e52e631954517567b025b070d5
janaSunrise <warriordefenderz@gmail.com>
2021-02-13 14:49:15 +0530

    Try converting into a python package

---
+++ project_quickstart/__init__.py
@@ -0,0 +1,5 @@
+__author__ = "Sunrit Jana"
+__email__ = "warriordefenderz@gmail.com"
+__version__ = "0.1.0-alpha"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2020 Sunrit Jana"
---
+++ setup.py
@@ -0,0 +1,76 @@
+import re
+from pathlib import Path
+
+import setuptools
+
+# Files
+BASE_DIR = Path(__file__).resolve().parent
+
+README = Path(BASE_DIR / "README.md").read_text()
+
+# Constants
+VERSION = re.search(
+    r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+    Path(BASE_DIR / "project_quickstart/__init__.py").read_text(),
+    re.MULTILINE
+).group(1)
+
+URL = "https://github.com/janaSunrise/project-quickstart"
+
+if not VERSION:
+    raise RuntimeError("VERSION is not set!")
+
+# Setup
+setuptools.setup(
+    name="ProjectQuickstart",
+    version=VERSION,
+
+    author="Sunrit Jana",
+    author_email="warriordefenderz@gmail.com",
+
+    description="An easy way to create projects manually, or using templates!",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+
+    url=URL,
+    project_urls={
+        "Documentation": URL,
+        "Issue tracker": f"{URL}/issues",
+    },
+
+    packages=setuptools.find_packages(
+        exclude=["tests", "tests.*", "tools", "tools.*"]
+    ),
+    install_requires=[
+        "requests==2.25.0",
+        "colorama==0.4.4",
+        "inquirer ==2.7.0"
+    ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+
+        "Development Status :: 2 - Pre-Alpha",
+
+        "Programming Language :: Python :: Implementation :: CPython",
+
+
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+
+        "Operating System :: OS Independent",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+
+        "Topic :: Scientific/Engineering :: Mathematics",
+
+        "Natural Language :: English",
+    ],
+
+    python_requires='>=3.7',
+)
